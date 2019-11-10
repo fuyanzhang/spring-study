62a043c4e8b0aa24d247a9e46c56ff5c623d6bd0
fuyanzhang <fuyanzhang@xiaomi.com>
2019-11-10 23:34:27 +0800

    first commit

---
+++ src/main/java/com/spring/study/StudyApplication.java
@@ -0,0 +1,22 @@
+package com.spring.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationImportSelector;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @ClassName StudyApplication
+ * @Author fuyanzhang
+ * @Date 2019/11/10 0010 下午 15:47
+ * @Description TODO
+ **/
+//@SpringBootApplication
+@Configuration
+@Import(AutoConfigurationImportSelector.class)
+public class StudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class);
+    }
+}
